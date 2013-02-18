b2b70102fb33390f09e38f7899274d86a3a0a6cd
Daniel Hjelm <github-d@hjelm.eu>
2013-02-18 11:19:07 +0800

    don't expose the whole problem documents through the API

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var Problem = mongoose.model('Problem');
 
 exports.problem_list = function(req, res){
-  Problem.find(function (err, problems) {
+  Problem.find({}, 'id name', function (err, problems) {
     if (err) {
       res.send(404, 'Could not get the problems from DB. Error: ' + err);
     }
@@ -12,12 +12,12 @@ exports.problem_list = function(req, res){
 };
 
 exports.problem = function(req, res){
-  Problem.find({_id: req.params.id}, function (err, problems) {
+  Problem.findOne({_id: req.params.id}, 'id name description', function (err, problem) {
     if (err) {
-      res.send(404, 'Could not get the problems from DB. Error: ' + err);
+      res.send(404, 'Could not get the problem from DB. Error: ' + err);
     }
-    if (problems.length == 1) {
-      res.send(problems[0]);
+    if (problem !== null) {
+      res.send(problem);
     } else {
       res.send(404, 'The requested problem could not be found');
     }
