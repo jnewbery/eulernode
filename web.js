ddd6d03067f80c43c6923d72877b9b2566f9d5b2
Daniel Hjelm <github-d@hjelm.eu>
2013-02-21 21:24:26 +0800

    switch from ejs to jade; add conf.js file for settings; add cookie-based session support

--- web.js
+++ web.js
@@ -3,18 +3,21 @@ var express = require('express'),
   path = require('path'),
   mongoose = require('mongoose'),
   routes = require('./routes'),
-  api_v1 = require('./routes/api_v1');
+  api_v1 = require('./routes/api_v1'),
+  conf = require('./conf');
 
 var app = express();
 
 app.configure(function () {
   app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: conf.session.secret }));
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
